44ac2925a3fce61f60714171c4533c7914c0e727
Laboratorio <laboratorio@lab.unigran>
2022-09-23 19:59:33 -0400

    prova

--- app/src/main/java/BancoDeDados/Entidades.java
+++ app/src/main/java/BancoDeDados/Entidades.java
@@ -1,1 +1,49 @@
 package com.software.provap1.entidades;
+
+public class Abastecimento {
+    private Integer id;
+    private Float quilometragemAtual;
+    private Float quantidadeAbastecida;
+    private String data;
+    private Float valor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getQuilometragemAtual() {
+        return quilometragemAtual;
+    }
+
+    public void setQuilometragemAtual(Float quilometragemAtual) {
+        this.quilometragemAtual = quilometragemAtual;
+    }
+
+    public Float getQuantidadeAbastecida() {
+        return quantidadeAbastecida;
+    }
+
+    public void setQuantidadeAbastecida(Float quantidadeAbastecida) {
+        this.quantidadeAbastecida = quantidadeAbastecida;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+}
