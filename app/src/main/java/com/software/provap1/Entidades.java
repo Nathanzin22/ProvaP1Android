63a0ddf481f7c6086ac4678d16834beb33309f75
Laboratorio <laboratorio@lab.unigran>
2022-09-23 19:36:25 -0400

    prova

--- app/src/main/java/com/software/provap1/Entidades.java
+++ app/src/main/java/com/software/provap1/Entidades.java
@@ -1,4 +1,44 @@
-package com.software.provap1.entidades;public class Abastecimento {
+package com.software.provap1.entidades;
+
+public class Abastecimento {
+    private Float quilometragemAtual;
+    private Float quantidadeAbastecida;
+    private String data;
+    private Float valor;
+
+    public Abastecimento(){
+
+    }
+
+    public Float getQuilometragemAtual() {
+        return quilometragemAtual;
+    }
+
+    public void setQuilometragemAtual(Float quilometragemAtual) {
+        this.quilometragemAtual = quilometragemAtual;
+    }
+
+    public Float getQuantidadeAbastecida() {
+        return quantidadeAbastecida;
+    }
+
+    public void setQuantidadeAbastecida(Float quantidadeAbastecida) {
+        this.quantidadeAbastecida = quantidadeAbastecida;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
 }
-
-
