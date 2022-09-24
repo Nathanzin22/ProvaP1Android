962ec69348f5565a8614f8d16519cde46b52339e
Laboratorio <laboratorio@lab.unigran>
2022-09-23 20:11:54 -0400

    prova

--- app/src/main/java/com/software/Provap1/BancoDeDados/DBHELPER.java
+++ app/src/main/java/com/software/Provap1/BancoDeDados/DBHELPER.java
@@ -1,4 +1,32 @@
 package com.software.Provap1.BancoDeDados;
 
-public class DBHELPER {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "ListaTelefonica", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String querySQL =
+                "create table listaAbastecimento(" +
+                        "id integer primary key autoincrement," +
+                        "quilometragemAtual float," +
+                        "quantidadeAbastecida float," +
+                        "data varchar(10)," +
+                        "valor float)";
+
+        sqLiteDatabase.execSQL(querySQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
 }
